47beb5ea9f0a8670dfe7c681668a513ebcfbe39a
heshanu3d <heshann@foxmail.com>
2019-01-07 13:15:29 +0800

    finish Game 1

---
+++ 5.x_Game_1/scripts/Coin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int CoinCount = 0;
+    void Start()
+    {
+        Coin.CoinCount++;
+    }
+
+    private void OnDestroy()
+    {
+        Coin.CoinCount--;
+        if(Coin.CoinCount <= 0)
+        {
+            GameObject Timer = GameObject.Find("LevelTimer");
+            //Destroy(Timer);
+            Object.Destroy(Timer);
+            GameObject[] FireworkSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach(GameObject go in FireworkSystem)
+            {
+                go.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.CompareTag("Player") )
+        {
+            Object.Destroy(gameObject);
+            //Destroy(gameObject);
+        }
+    }
+}
---
+++ 5.x_Game_1/scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float MaxTime = 60f;
+
+    [SerializeField]
+    private float CountDown = 0;
+
+    void Awake()
+    {
+        CountDown = MaxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown -= Time.deltaTime;
+        if(CountDown < 0 )
+        {
+            Coin.CoinCount = 0;
+            SceneManager.LoadScene("level__01");
+        }
+    }
+}
